0c1a14077605642fe8ac51e9fa88efe1577822a6
Mark Oates <euph_boy@hotmail.com>
2015-06-27 19:00:13 -0400

    Improved HUD styling

--- src/park.h
+++ src/park.h
@@ -50,7 +50,7 @@ public:
 	HUD(ALLEGRO_DISPLAY *display, Park *park)
 		: display(display)
 		, hud_overlay(al_create_sub_bitmap(al_get_backbuffer(display), 0, 0, al_get_display_width(display), al_get_display_height(display)))
-		, font(al_load_font("data/fonts/Dense-Regular.otf", 40, 0))
+		, font(al_load_font("data/fonts/Dense-Regular.otf", 36, 0))
 		, icons(al_load_font("data/fonts/FontAwesome.otf", 30, 0))
 		, park(*park)
 	{
@@ -63,15 +63,15 @@ public:
 
 		int lineheight = al_get_font_ascent(font);
 		int y_cursor = lineheight;
-		int i=0;
 		ALLEGRO_COLOR color = al_color_name("white");
-		al_draw_textf(font, color, 10, lineheight*i++, 0, "Visitors: %d", park.num_visitors);
-		al_draw_textf(font, color, 10, lineheight*i++, 0, "$: %d", park.money);
-		al_draw_textf(font, color, 10, lineheight*i++, 0, "visitor_happiness: %d", park.visitor_happiness);
-		al_draw_textf(font, color, 10, lineheight*i++, 0, "employees: %d", park.employees);
-		al_draw_textf(font, color, 10, lineheight*i++, 0, "num_assets: %d", park.assets.size());
 
-		//al_set_target_bitmap(al_get_backbuffer(display));
+		al_draw_filled_rounded_rectangle(10, 10, 350, 60, 16, 12, al_map_rgba_f(0, 0, 0, 0.2));
+		al_draw_textf(font, color, 20, 20, 0, "visitors: %d", park.num_visitors);	
+		al_draw_textf(font, color, 200, 20, 0, "$$$: %d", park.money);
+		
+		al_draw_filled_rounded_rectangle(al_get_display_width(display)-350, 10, al_get_display_width(display)-10, 60, 16, 12, al_map_rgba_f(0, 0, 0, 0.2));
+		al_draw_textf(font, color, al_get_display_width(display)-200, 20, ALLEGRO_ALIGN_RIGHT, "happiness: %d", park.visitor_happiness);
+		al_draw_textf(font, color, al_get_display_width(display)-20, 20, ALLEGRO_ALIGN_RIGHT, "employees: %d", park.employees);
 	}
 };
 
